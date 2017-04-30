6b3d91cc66e51e21d2e75649f67a28c5c00a1a6f
Brian Bauer <bbauer@e1z2r5p6.42.us.org>
2017-04-30 11:07:24 -0700

    fixed arg parsing errer with end quote mark being passed as input

--- src/minishell_parse_args.c
+++ src/minishell_parse_args.c
@@ -6,7 +6,7 @@
 /*   By: bbauer <bbauer@student.42.us.org>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/04/28 14:27:16 by bbauer            #+#    #+#             */
-/*   Updated: 2017/04/28 14:27:22 by bbauer           ###   ########.fr       */
+/*   Updated: 2017/04/30 10:40:55 by bbauer           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,24 +22,31 @@ void			separate_multiple_args(char ***args, char **line)
 {
 	size_t	i;
 	int		l_begin;
+	int		end_quote;
 
 	trim_and_free(line);
 	i = 0;
+	end_quote = 0;
 	while (line && *line && i < ft_strlen(*line))
 	{
 		l_begin = i;
-		while ((*line)[i] && (*line)[i] != ' ')
+		while ((*line)[i] && (*line)[i] != ' ' && !end_quote)
 		{
 			if ((*line)[i] == '\"')
 			{
 				i++;
+				end_quote = 1;
 				while ((*line)[i] != '\"')
 					i++;
 			}
 			i++;
 		}
-		if ((*line)[i] == '\0' || (*line)[i] == ' ')
+		if ((*line)[i] == '\0' || (*line)[i] == ' ' || end_quote)
+		{
+			end_quote = 0;
 			copy_to_args_lst(args, line, i, l_begin);
-		i++;
+		}
+		if ((*line)[i] == ' ')
+			i++;
 	}
 }
